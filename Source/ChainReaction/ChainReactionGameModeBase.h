e9ddf85c2db95dc1431aa54c4f7b8107505ffbf2
Slemelie <73103451+Slemelie@users.noreply.github.com>
2021-10-15 16:32:38 +0200

    Initial commit

---
+++ Source/ChainReaction/ChainReactionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChainReactionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAINREACTION_API AChainReactionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
